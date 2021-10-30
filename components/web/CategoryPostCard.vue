e9a7e7af322f7482f3a120b5c93c987d232b96a1
magusidris <magusidris@gmail.com>
2021-10-31 04:17:09 +0700

    update views in slug category

---
+++ components/web/CategoryPostCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by2">
+          <nuxt-img
+            :src="`/storage/posts/`+post.image"
+            alt="post.title"
+            preset="web"
+            sizes="sm:355px md:320px lg:480px" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-size-6">{{ post.title }}</p>
+          </div>
+        </div>
+        <div class="content">
+
+          <p class="is-size-7">
+            <time :datetime="post.created_at">
+              <b-icon
+                pack="fas"
+                icon="calendar"
+                size="is-small">
+              </b-icon>
+              {{ post.created_at }}
+            </time>
+            <b-icon
+              class="ml-4"
+              pack="fas"
+              icon="eye"
+              size="is-small">
+            </b-icon>
+            {{ viewCount }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    viewCount() {
+      return this.post.views.length
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .title {
+    height: 4rem
+  }
+</style>
--- pages/category/_slug.vue
+++ pages/category/_slug.vue
@@ -2,12 +2,13 @@
   <section class="section">
     <div class="container is-max-widescreen">
       <h1 class="title">
-        Category : {{ category.name }}
+        Category : <span class="has-text-dark-green">{{ category.name }}</span>
       </h1>
       <div class="columns is-multiline">
         <div class="column is-4" v-for="post in category.posts" :key="post.id">
           <nuxt-link :to="{ name: 'slug', params: {slug: post.slug} }">
-            <web-post-card :post="post" />
+            <web-category-post-card :post="post" />
+            <!-- {{ viewCount }} -->
           </nuxt-link>
         </div>
       </div>
@@ -18,7 +19,6 @@
 <script>
 import { mapState } from 'vuex'
 export default {
-
   // computed
   computed: {
 
