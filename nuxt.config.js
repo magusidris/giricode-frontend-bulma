0649108ee494a09ddd37ad3c2bdc9e2b103eac38
magusidris <magusidris@gmail.com>
2021-11-05 00:56:26 +0700

    edit base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,7 +129,7 @@ export default {
       'jwt': {
         provider: 'laravel/jwt',
         scheme: 'refresh',
-        url: process.env.BASE_URL && 'http://localhost:2021',
+        url: pkg.base_url || 'http://localhost:2021',
         endpoints: {
           login: {
             url: '/api/v1/auth/login',
