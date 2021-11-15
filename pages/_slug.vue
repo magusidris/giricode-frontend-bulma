92a37215e5cfd6c037be3047cb38ee7d6306a138
nuxtcraft <p31202102425@mhs.dinus.ac.id>
2021-11-15 22:35:17 +0700

    update return error in storeVisitor

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -53,6 +53,8 @@ export default {
   },
   async asyncData({store, params}) {
     await store.dispatch('web/post/storeVisitor', params.slug)
+      .catch(error=> console.log(error))
+
     await store.dispatch('web/post/updateSlug', {slug: params.slug})
   },
   head() {
