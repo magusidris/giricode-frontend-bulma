fed6e0cf1aad3a80d885fdfd0afbdd9a72d73f32
nuxtcraft <p31202102425@mhs.dinus.ac.id>
2021-11-16 08:30:33 +0700

    update animation card

--- components/web/PostCard.vue
+++ components/web/PostCard.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="card blog">
-    <div class="columns is-multiline is-gapless">
-      <div class="column card-image is-full">
-        <figure class="image is-4by2">
-          <nuxt-img
-            :src="`/storage/posts/`+post.image"
-            alt="post.title"
-            preset="web"
-            sizes="sm:355px md:320px lg:480px" />
-        </figure>
+    <div class="card-image">
+      <figure class="image is-4by2">
+        <nuxt-img
+          :src="`/storage/posts/`+post.image"
+          alt="post.title"
+          preset="web"
+          sizes="sm:355px md:320px lg:480px" />
+      </figure>
+    </div>
+    <div class="card-content px-4">
+      <div class="media">
+        <div class="media-content">
+          <span class="tag is-light mr-2" :class="tag.color.value" v-for="tag in post.tags" :key="tag.id" v-html="tag.name" />
+          <p class="blog-title title is-4" v-html="post.title" />
+        </div>
       </div>
-      <div class="mx-3 my-3 column status card-content is-full has-text-grey">
-        <div class="content pr-5">
-          <p class="is-size-7 has-text-weight-medium">
-            <span class="tag is-light mr-2" :class="tag.color.value" v-for="tag in post.tags" :key="tag.id" v-html="tag.name" />
-          </p>
-          <p class="blog-title title is-size-6" v-html="post.title" />
-          <p class="is-size-7">
-            <time :datetime="post.created_at">
-              <b-icon
-                pack="fas"
-                icon="calendar"
-                size="is-small">
-              </b-icon>
-              {{ post.created_at }}
-            </time>
-
+      <div class="content pr-5">
+        <p class="is-size-7">
+          <time :datetime="post.created_at">
             <b-icon
-              class="ml-4"
               pack="fas"
-              icon="eye"
+              icon="calendar"
               size="is-small">
             </b-icon>
-            {{ post.views_count }}
+            {{ post.created_at }}
+          </time>
 
-          </p>
-        </div>
+          <b-icon
+            class="ml-4"
+            pack="fas"
+            icon="eye"
+            size="is-small">
+          </b-icon>
+          {{ post.views_count }}
+
+        </p>
       </div>
     </div>
   </div>
@@ -51,3 +51,6 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/web/PostCardbackup.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card blog">
+    <div class="columns is-multiline is-gapless">
+      <div class="column card-image is-full">
+        <figure class="image is-4by2">
+          <nuxt-img
+            :src="`/storage/posts/`+post.image"
+            alt="post.title"
+            preset="web"
+            sizes="sm:355px md:320px lg:480px" />
+        </figure>
+      </div>
+      <div class="mx-3 my-3 column status card-content is-full has-text-grey">
+        <div class="content pr-5">
+          <p class="is-size-7 has-text-weight-medium">
+            <span class="tag is-light mr-2" :class="tag.color.value" v-for="tag in post.tags" :key="tag.id" v-html="tag.name" />
+          </p>
+          <p class="blog-title title is-size-6" v-html="post.title" />
+          <p class="is-size-7">
+            <time :datetime="post.created_at">
+              <b-icon
+                pack="fas"
+                icon="calendar"
+                size="is-small">
+              </b-icon>
+              {{ post.created_at }}
+            </time>
+
+            <b-icon
+              class="ml-4"
+              pack="fas"
+              icon="eye"
+              size="is-small">
+            </b-icon>
+            {{ post.views_count }}
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.blog-title:hover {
+  color: $dark-green;
+}
+</style>
